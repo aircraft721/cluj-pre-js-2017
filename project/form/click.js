507ce9ab66cee25a9c7da12686623f59ddae5d8b
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-08-16 10:23:23 +0300

    added the project files

---
+++ project/form/click.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function(){
+    $('.hover-ul li').hover(function(){
+        $(this).css('border-bottom','10px solid black');
+    }),function(){
+        $(this).css('border-bottom','0');
+    }
+    
+});*/
