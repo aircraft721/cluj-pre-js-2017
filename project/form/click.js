7798fc398d489d2fa1e8b821e333709efd472a2c
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-08-17 09:15:11 +0300

    removed the project with bootstrap

--- project/form/click.js
+++
@@ -1,8 +0,0 @@
-/*$(document).ready(function(){
-    $('.hover-ul li').hover(function(){
-        $(this).css('border-bottom','10px solid black');
-    }),function(){
-        $(this).css('border-bottom','0');
-    }
-    
-});*/
