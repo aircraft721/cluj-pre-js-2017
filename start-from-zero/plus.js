b78af26271a0600fb2a50f437f9c34d8b9a358c0
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-08-17 17:10:13 +0300

    finished most of the things

---
+++ start-from-zero/plus.js
@@ -0,0 +1,11 @@
+
+var plus = document.querySelector('.plus-sign');
+
+plus.addEventListener('click',function(){
+    if (plus.textContent = '+'){
+        plus.textContent = '-';
+    }else{
+        plus.textContent = '+';
+    }
+})
+
