1c929498dd690913caec13239e820aef71ea006b
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-08-22 23:25:35 +0300

    added the CandidateDetails component for the evaluation page

--- start-from-zero/Javascript/evaluation.js
+++ start-from-zero/Javascript/evaluation.js
@@ -21,10 +21,55 @@ const TableHeadElement = function(options){
     const elements = [{name:'Nume'},{name:'Tehnologie'},{name:'Nivel'}, {name:''}];
     elements.forEach(function(n){
         tableHead.push(`<th class='table-head-element'>${n.name}</th>`);
-    })
-    return `
-        ${tableHead.join('')}
-    `;
+    });
+    return tableHead.join('');
+}
+
+const CandidateDetailsForm = function(options){
+    const candidateDetails = [];
+    const details = [
+
+        {
+            name:'Dan Constantinescu',
+            language: 'Javascript',
+            level:'Junior'
+        },
+        {
+            name:'Adolf H',
+            language: 'SS',
+            level:'Master'
+        },
+        {
+            name:'Fiodor D',
+            language: 'Russian',
+            level:'Master'
+        },
+        {
+            name:'Reddit',
+            language: 'Ruby',
+            level:'Master'
+        },
+    ];
+
+   
+
+
+    details.map(function(n){
+        
+        candidateDetails.push(
+        `<tr class="table-body-tr">
+            <td>${n.name}</td>
+            <td>${n.language}</td>
+            <td>${n.level}</td>
+            <td class='detalii'>Detalii 
+                <a href="#" class="button-toggle plus-button">
+                    <span class="minus-toggle"></span>
+                </a>
+            </td>
+        </tr>`);
+        
+    });
+    return candidateDetails.join('');
 }
 
 
@@ -42,47 +87,7 @@ const EvaluationPageTable = function(){
                             </tr>
                         </thead>
                         <tbody class='table-body'>
-                            <tr class='table-body-tr'>
-                                <td>Popescu Adrian</td>
-                                <td>Javascript</td>
-                                <td>Mid 2</td>
-                                <td class='detalii'>Detalii
-                                    <a href="#" class="button-toggle plus-button">
-                                        <span class="minus-toggle"></span>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr class='table-body-tr'>
-                                <td>Popescu Adrian</td>
-                                <td>Javascript</td>
-                                <td>Mid 2</td>
-                                <td class='detalii'>Detalii 
-                                    <a href="#" class="button-toggle plus-button">
-                                        <span class="minus-toggle"></span>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr class='table-body-tr'>
-                                <td>Popescu Adrian</td>
-                                <td>Javascript</td>
-                                <td>Mid 2</td>
-                                <td class='detalii'>Detalii 
-                                    <a href="#" class="button-toggle plus-button">
-                                        <span class="minus-toggle"></span>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr class='table-body-tr'>
-                                <td>Popescu Adrian</td>
-                                <td>Javascript</td>
-                                <td>Mid 2</td>
-                                <td class='detalii'>Detalii 
-                                    <a href="#" class="button-toggle plus-button">
-                                        <span class="minus-toggle"></span>
-                                    </a>
-                                </td>
-
-                            </tr>
+                            ${CandidateDetailsForm()}
                         </tbody>
                     </table>
                 </div>
