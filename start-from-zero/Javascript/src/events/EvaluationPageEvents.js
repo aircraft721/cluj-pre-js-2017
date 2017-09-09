071f4c59d400d2604bee015ea90dee013bd56c8e
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-09-09 13:44:20 +0300

    details button finally working

--- start-from-zero/Javascript/src/events/EvaluationPageEvents.js
+++ start-from-zero/Javascript/src/events/EvaluationPageEvents.js
@@ -1,16 +1,13 @@
 (function(){
     windowObjectWrap.EvaluationPageEvents = function() {
         this.initEvents = function(build) {
-            
             const toggle = document.querySelectorAll('.plus-button');
             for(var detailsButton of toggle){
                 detailsButton.addEventListener('click',function(e){
                     e.preventDefault();
-                    var displayData = e.target.parentNode.parentNode.parentNode.dataset;
-                    
-                    console.log(displayData);
-                    const accordion = document.getElementById('accordion');
-                    accordion.classList.toggle('hidethis');
+                    const displayData = e.target.parentNode.parentNode.parentNode.dataset.id;
+                    const elem = document.querySelector(`div[data-id="${displayData}"]`)
+                    elem.classList.toggle('hidethis');
                 });
             }
 
